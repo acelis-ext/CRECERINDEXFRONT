e3e3c8f265badaf054932f55a74fc0d30f9e98be
Adrian Celis <desarrollador3-ext@CRECERSEGUROS.PE>
2025-09-24 11:55:58 -0500

    feat: final de desarrollo

--- src/environments/environments.prod.ts
+++ src/environments/environments.prod.ts
@@ -3,6 +3,6 @@ export const environment = {
 
   apiBaseUrl: 'https://crecer-indexapi.azurewebsites.net/api/',
   // apiBaseUrl: 'https://localhost:7144/api/',
-  CAPTCHA_KEY: '6LdN6boZAAAAAAcVzFozDB2fpZk_-UW6mp8BP1ux' // clave de prueba pública de Google
+  CAPTCHA_KEY: '6LfdwNArAAAAAMf0LQIrbfg27Qvqsgf-w3OOHZDS' // clave de prueba pública de Google
 
 };
