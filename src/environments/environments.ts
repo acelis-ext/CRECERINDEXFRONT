ac0d6676430d90f6ee39aea28dc50f8cc40b842b
Adrian Celis <desarrollador3-ext@CRECERSEGUROS.PE>
2025-12-11 10:22:31 -0500

    cambios finales visuales

--- src/environments/environments.ts
+++ src/environments/environments.ts
@@ -1,11 +1,18 @@
 export const environment = {
   production: false,
 
-  // apiBaseUrl: 'https://crecer-indexapi.azurewebsites.net/api/',
-  // apiBaseUrl: 'https://localhost:7144/api/',
-  //por cambios de vnet privada se va dejar vacio el apiBaseUrl SAME-ORIGIN REQUEST
-      // apiBaseUrl: 'https://localhost:7144',  ya no va tener el prefijo api para cuando este desplegado a prod
+  //desarrollo local
+  // apiBaseUrl: 'https://localhost:7144',
 
+
+  //antiguo QA sin seguridad
+  // apiBaseUrl: 'https://crecer-indexapi.azurewebsites.net/api/',
+
+
+  //ESTE ES PARA HACER LOS PASE A QA Y A PROD
+  //produccion
+  ////por cambios de vnet privada se va dejar vacio el apiBaseUrl SAME-ORIGIN REQUEST
+  //// apiBaseUrl: 'https://localhost:7144',  ya no va tener el prefijo api para cuando este desplegado a prod
   apiBaseUrl: '',
   CAPTCHA_KEY: '6LfdwNArAAAAAMf0LQIrbfg27Qvqsgf-w3OOHZDS' // clave de prueba pública de Google
   
